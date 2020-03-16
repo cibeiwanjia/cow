8c028d069d8489ae16f39d6f1fc75403123c2de4
cibeiwanjia <48764703+cibeiwanjia@users.noreply.github.com>
2020-03-16 15:09:50 +0800

    version2

--- login.vue
+++ login.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">奶牛围产期行为识别系统</h3>
+        <h3 class="title">奶牛围产期行为识别系统1</h3>
       </div>
 
       <el-form-item prop="username">
